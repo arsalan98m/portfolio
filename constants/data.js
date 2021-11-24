3c59a1e994a40d670cc84aa83fdd166f586fd53e
arsalan manzoor <arsalan1998m@gmail.com>
2021-11-25 00:30:44 +0500

    personal portfolio

---
+++ constants/data.js
@@ -0,0 +1,103 @@
+const userData = {
+  githubUsername: 'arsalan98m',
+  name: 'Muhammad Arsalan Manzoor',
+  designation: 'Front End Developer',
+  avatarUrl: '/avatar.jpg',
+  email: 'arsalan90m@gmail.com',
+  phone: '+92 3112190711',
+  address: 'Karachi, Pakistan.',
+  projects: [
+    {
+      title: 'Modern Landing Page',
+      link: 'https://arsalan98m.github.io/modern-landing-page/',
+      imgUrl: '/tmk.png',
+    },
+    {
+      title: 'Netflix Clone',
+      link: 'https://signapplication-ba353.web.app/',
+      imgUrl: '/netflix.png',
+    },
+    {
+      title: 'Airbnb Clone',
+      link: 'https://airbnb-clone-six-kappa.vercel.app/',
+      imgUrl: '/airbnb.png',
+    },
+    {
+      title: 'Slack Clone',
+      link: 'https://slack-clone-b7411.web.app/',
+      imgUrl: '/slack-2.0.png',
+    },
+    {
+      title: 'Tesla Front Page Clone',
+      link: 'https://tesla-clone-331cd.web.app/',
+      imgUrl: '/tesla.png',
+    },
+    {
+      title: 'Whatsapp Clone',
+      link: 'https://whatsapp-clone-49ffa.web.app/',
+      imgUrl: '/whatsapp.png',
+    },
+    {
+      title: 'Twitter Clone',
+      link: 'https://twitter-clone-a9675.web.app/',
+      imgUrl: '/twitter.png',
+    },
+    {
+      title: 'Flicqx Clothing(Ecommerce)',
+      link: 'https://www.flicqx.com/',
+      imgUrl: '/flicqx.png',
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building software products and web applications that impact millions of lives",
+    description: [
+      `I've been developing front end web application since I was 1.5 years old. I didn't know what software development meant at that time because the term was not coined back then.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in JavaScript ,React, NextJS, Firebase, Redux, Node.js, Bootstrap, Tailwind, Material UI, and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Stack overflow!), I consider myself a Ninja developer.`,
+    ],
+    currentProject: 'Dakters',
+    currentProjectUrl: 'http://dakters.com/',
+  },
+  experience: [
+    {
+      title: 'Software Developer',
+      company: 'Codebase Technology',
+      year: '2021',
+      companyLink: 'https://www.codebtech.com/',
+      desc: 'Developed a Doctors Virtual Consultation App and Currently Working on Uber Like Project.',
+    },
+
+    {
+      title: 'Web and Mobile App Development',
+      company: 'PIAIC',
+      year: '2020',
+      companyLink: 'http://piaic.org/',
+      desc: 'Developed Web and Mobile Apps.',
+    },
+    {
+      title: 'Diploma',
+      company: 'Aptech',
+      year: '2019',
+      companyLink: 'https://aptech-education.com.pk/',
+      desc: 'Major in Computer Science and Software Development ',
+    },
+    {
+      title: 'High School',
+      company: 'Govt. High School, Larkana',
+      year: '2018',
+      companyLink: 'https://www.biselrk.edu.pk/',
+      desc: 'Pre-engineering',
+    },
+  ],
+  resumeUrl:
+    'https://drive.google.com/file/d/19nOtANYFtUkGyA6orLZI6VYi8ktGL6W6/view?usp=sharing',
+  socialLinks: {
+    twitter: 'https://twitter.com/arsalan90m',
+    linkedin: 'https://www.linkedin.com/in/muhammad-arsalan-manzoor-4088479b/',
+    github: 'https://github.com/arsalan98m',
+    facebook: 'https://www.facebook.com/profile.php?id=100006634666984',
+  },
+};
+
+export default userData;
